f007be55ddbf3b4494ba99935fd14be1d7b74398
designUon <design_uon@naver.com>
2023-06-02 11:56:57 +0900

    230602 team git test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Route, Routes} from 'react-router-dom'
+import Home from './page/eunhee/Home';
+import About from './page/sl/About';
+import Story from './page/yh/Story';
+import Mypage from './page/dw/Mypage';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>team-git-test-230602</h3>
+        <Routes>
+          
+          {/* 은희 */}
+          <Route path='/' element={<Home/>}/>
+
+          {/* 슬린 */}
+          <Route path='/about' element={<About/>}/>
+          
+          {/* 영환 */}
+          <Route path='/story' element={<Story/>}/>
+          
+          {/* 대욱 */}
+          <Route path='/mypage' element={<Mypage/>}/>
+
+        </Routes>
     </div>
   );
 }
